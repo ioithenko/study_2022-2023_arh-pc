6505c2bf1c4b233504f6de225af345def5b737f9
Ирина Ищенко <iishchenko99@gmail.com>
2022-11-09 14:21:40 +0300

    feat(main): add files lab-5

---
+++ labs/lab05/lab5.asm
@@ -0,0 +1,15 @@
+; lab5.asm
+SECTION .data ; Начало секции данных
+name: DB 'Ishchenko Irina',10 ; 'Ishchenko Irina' плюс символ перевода строки
+nameLen: EQU $-name ; Длина строки name
+SECTION .text ; Начало секции кода
+GLOBAL _start
+_start: ; Точка входа в программу
+mov eax,4 ; Системный вызов для записи (sys_write)
+mov ebx,1 ; Описатель файла '1' - стандартный вывод
+mov ecx,name ; Адрес строки name в ecx
+mov edx,nameLen ; Размер строки name
+int 80h ; Вызов ядра
+mov eax,1 ; Системный вызов для выхода (sys_exit)
+mov ebx,0 ; Выход с кодом возврата '0' (без ошибок)
+int 80h ; Вызов ядра
